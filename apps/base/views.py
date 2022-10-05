f66f1c2868ba8ef851ed9242b1758387680eb4ea
VichiesUA <stasyatsyn@gmail.com>
2022-10-05 19:39:00 +0300

    push to school

--- apps/base/views.py
+++ apps/base/views.py
@@ -3,8 +3,9 @@ from django.http import HttpRequest, HttpResponse
 from apps.base.services.get_random_user import get_random_user
 
 
-def index(request: HttpRequest, name=None) -> HttpResponse:
-    if name is None:
-        return HttpResponse(f'Hello {get_random_user()}')
-    else:
-        return HttpResponse(f'Hallo {name}')
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(f'Hello {get_random_user()}')
+
+
+def create(request: HttpRequest, name='<name>'):
+    return HttpResponse(f'Hallo {name}')
