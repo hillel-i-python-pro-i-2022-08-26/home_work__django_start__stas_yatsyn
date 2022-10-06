b1d68537963ab5aa0c2cf8bb55561366fd744258
VichiesUA <stasyatsyn@gmail.com>
2022-10-06 11:09:58 +0300

    Push final version1

--- apps/base/views.py
+++ apps/base/views.py
@@ -6,4 +6,4 @@ from .services.get_random_user import get_random_user
 def index(request: HttpRequest, name: str | None = None) -> HttpResponse:
     if name is None:
         name = get_random_user()
-    return HttpResponse(f"Hello, {name} ")
+    return HttpResponse(f"Hello,  {name} ")
